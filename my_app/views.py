cdd5cb0872ee266a644e1b0a10ba20021501527c
sharanya39 <sharanya@LAPTOP-511UED4J.>
2024-08-13 22:49:36 -0500

    update coding added

--- my_app/views.py
+++ my_app/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render ,redirect
 from django.http import HttpResponse
 from .models import Datas
 # Create your views here.
 
 def home(request):
+    mydata=Datas.objects.all() 
+    if(mydata!=""):
+       return render(request,"home.html",{'datas':mydata})
+    else:  
+        return render(request,"home.html")
+
+def addData(request):
     if request.method=="POST":
         name=request.POST['name']
         age=request.POST['age']
@@ -18,8 +25,35 @@ def home(request):
         obj.Contact=contact
         obj.Mail=mail
         obj.save()
+        mydata=Datas.objects.all()
+        return redirect('home')
+    return render(request,'home.html')
 
-    return render(request,"home.html")
+def updateData(request,id):
+    mydata=Datas.objects.get(id=id)
+    if request.method=='POST':
+        name=request.POST['name']
+        age=request.POST['age']
+        address=request.POST['address']
+        contact=request.POST['contact']
+        mail=request.POST['mail']
+
+        mydata.Name=name
+        mydata.Age=age
+        mydata.Address=address
+        mydata.Contact=contact
+        mydata.Mail=mail
+        mydata.save()
+        return redirect('home')
+        
+    return render(request,'update.html',{'data':mydata})
+
+def deleteData(request,id):
+    mydata=Datas.objects.get(id=id)
+    mydata.delete()
+    return redirect('home')
+        
+    
 
 
 def register(request):
--- my_project/urls.py
+++ my_project/urls.py
@@ -20,9 +20,10 @@ from my_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home),
-    #path('Tour',views.Tour),
-    path('register',views.register)
+    path('',views.home,name="home"),
+    path('addData',views.addData,name="addData"),
+    path('updateData/<int:id>',views.updateData,name="updateData"),
+    path('deleteData/<int:id>',views.deleteData,name="deleteData"),
 
     
 ]
